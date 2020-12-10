905eaf4cb8a45cb5d2c6fcc3896e7333ad5799af
harumonia <zxjlm233@163.com>
2020-12-10 15:47:58 +0800

    rm .idea

--- .idea/fileTemplates/internal/Python Script.py
+++
@@ -1,8 +0,0 @@
-"""
-@project : ${PROJECT_NAME}
-@author  : harumonia
-@mail   : zxjlm233@163.com
-@ide    : ${PRODUCT_NAME}
-@time   : ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}:${SECOND}
-@description: None
-"""
