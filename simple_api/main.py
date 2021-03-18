ff3227ec15598b5e3a6e848fe281f4e1826ab80d
harumonia <zxjlm233@163.com>
2021-03-18 17:04:05 +0800

    :bug: change filename storage way

--- simple_api/main.py
+++ simple_api/main.py
@@ -1,7 +1,7 @@
 import re
 
 import uvicorn
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Form
 
 from utils import single_font_to_pic
 
@@ -9,15 +9,13 @@ app = FastAPI()
 
 
 @app.post('/api/special_for_printed_digits/')
-async def special_for_printed_digits(file: UploadFile = File(...)):
+async def special_for_printed_digits(file: UploadFile = File(...), filename: str = Form(...)):
     """
     针对数字进行特化
     Returns:
 
     """
-    filename = re.sub('[（(）) ]', '', file.filename)
-
-    res = single_font_to_pic(file.filename, file.file.read())
+    res = single_font_to_pic(filename, file.file.read())
     return {'code': 200, 'font_dict': {foo['name']: int(foo['ocr_result']) for foo in res}}
 
 
