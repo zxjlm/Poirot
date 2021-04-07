3aa80f31178b2606d27554e27d8c267105b0bd96
harumonia <zxjlm233@163.com>
2021-04-07 19:06:55 +0800

    use semantic ui

--- prirot-frontend/src/index.js
+++ prirot-frontend/src/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import PMenu from "./PMenu";
 import reportWebVitals from "./reportWebVitals";
+
+// const styleLink = document.createElement("link");
+// styleLink.rel = "stylesheet";
+// styleLink.href =
+//   "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
+// document.head.appendChild(styleLink);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <PMenu />
   </React.StrictMode>,
   document.getElementById("root")
 );
