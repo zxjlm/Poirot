e1dbeb8624b90bcb20a424a83ae8a73ab0723e2e
SwordHeart <zxjlm233@163.com>
2021-04-09 18:22:46 +0800

    main ui frame

--- poirot-frontend/src/index.js
+++ poirot-frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import PMenu from "./PMenu";
+import MenuAppBar from "./compoments/PMenu";
 import reportWebVitals from "./reportWebVitals";
 
 // const styleLink = document.createElement("link");
@@ -12,7 +11,7 @@ import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <PMenu />
+    <MenuAppBar />
   </React.StrictMode>,
   document.getElementById("root")
 );
