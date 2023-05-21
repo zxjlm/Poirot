efaf696725a7a6735c5b16abbbdbbeb53d3e121d
harumonia <zxjlm233@gmail.com>
2023-05-21 16:38:17 +0800

    fix: update fastapi route

--- backend/app/app/api/api.py
+++ backend/app/app/api/api.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import utils
+from app.api.api_v1.endpoints.font import font_api_router
 
 api_router = APIRouter()
-api_router.include_router(utils.main_api_router, prefix="/api", tags=["utils"])
+api_router.include_router(font_api_router, prefix="/api", tags=["utils"])
 # api_router.include_router(items.router, prefix="/items", tags=["items"])
